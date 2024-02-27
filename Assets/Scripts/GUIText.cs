2ecb608e22f6546e5af2a4b36dcbf2f08e6b92e8
JinhyeonYu <williamjinhyeon1@gmail.com>
2024-02-27 11:52:12 +0900

    Fire Sampler Added and shader scripted. Scene Settings changed to not rendering skybox

--- Assets/Scripts/GUIText.cs
+++ Assets/Scripts/GUIText.cs
@@ -7,15 +7,21 @@ public class GUIText : MonoBehaviour
     [SerializeField]
     private string TextForGizmo = "";
 
+    [SerializeField]
+    Vector3 AdjustByCustom = Vector3.zero;
+
+    [SerializeField]
+    int FontSize = 20;
+
     private void OnDrawGizmos()
     {
         Texture2D texture = Resources.Load<Texture2D>("TextureForGUI");
         GUIStyle style = new GUIStyle();
         style.normal.textColor = Color.white;
         style.normal.background = texture;
-        style.fontSize *= 2;
+        style.fontSize = FontSize;
 
-        Vector3 adjustVector = transform.right + transform.up;
+        Vector3 adjustVector = transform.right + transform.up + AdjustByCustom;
         UnityEditor.Handles.Label(transform.position + adjustVector, TextForGizmo, style);
     }
 
